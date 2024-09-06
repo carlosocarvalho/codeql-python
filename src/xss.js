1341e50be513770b356145e9c049d6a1884b7e8b
Carlos Carvalho <contato@carlosocarvalho.com.br>
2024-09-06 17:16:51 +0000

    fix: add script

--- src/xss.js
+++ src/xss.js
@@ -1,7 +1,5 @@
 // This is an intentionaly vulnerable Javascript file used for testing, do not use or deploy it anywhere.
 
-
-
 var username = "jonas"
 var password = "1289212"
 
@@ -16,3 +14,5 @@ var username = urlParams.get('username');
 var unsafe_div = window.document.getElementById("vulnerable-div");
 // here's an XSS:
 unsafe_div.innerHTML = "Hello to you " + username;
+
+
