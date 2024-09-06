45c2069e2568f755dc2e4a403bc2dd24f8e3eb7c
Carlos Carvalho <contato@carlosocarvalho.com.br>
2024-09-06 16:35:15 +0000

    fix: add

--- src/xss.js
+++ src/xss.js
@@ -1,4 +1,17 @@
 // This is an intentionaly vulnerable Javascript file used for testing, do not use or deploy it anywhere.
+const _ = require('lodash@4.17.4');
+
+
+var username = "jonas"
+var password = "1289212"
+var secret  = Buffer.from('Javascript').toString('base64')
+
+console.log(eval(secret))
+
+const { exec } = require('child_process');
+const userInput = 'ls'; 
+exec(userInput, (error, stdout) => console.log(stdout)); // Inseguro: entrada do usuário pode ser um comando malicioso
+
 
 var urlParams = new URLSearchParams(window.location.search);
 var username = urlParams.get('username');
